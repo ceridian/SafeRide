b03f64d13f2545d20d268c342366b6f782ec671f
Kathleen Van Vleet <klane2@WASH-klane2.local>
2015-04-11 10:24:38 -0400

    test

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexs);
+app.use('/', index);
 app.use('/users', users);
 
 app.get('/404', function(req, res, next){
