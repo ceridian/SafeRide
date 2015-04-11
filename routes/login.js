1a962a4bf961d6fadba615ebcc2f34ab13b0241f
Kathleen Van Vleet <klane2@WASH-klane2.local>
2015-04-11 11:35:02 -0400

    added login api

---
+++ routes/login.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+var path = require('path');
+//var a = require('../lib/auth.js');
+
+router.post('/', function(req, res) {
+  var body = req.body;
+  a.checkUser(body, function(err, obj){
+    if(err){
+      res.msg(err);
+      res.send(500);
+    }else{
+      if(obj.status == 'ok'){
+        req.session.user_id = obj.user;
+        res.send(obj);
+      }else{
+        res.send(obj);
+      }
+    }
+  })
+});
+
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
+
+module.exports = router;
