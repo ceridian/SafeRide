32b2ddcfb459de4c32204b2efafcb0ae4ad19d29
ceridian <jacob.vanvleet@gmail.com>
2015-04-12 08:59:19 -0400

    test

--- public/js/lib/app.js
+++ public/js/lib/app.js
@@ -75,7 +75,7 @@
 			templateUrl: 'temps/searchBar.html',
 			controller: function($scope, $rootScope, $http, $location){
 				$scope.obj = {};
-				$rootScope.routeObj = "";
+				$rootScope.routeObj = {};
 				$scope.submit = function(obj){
 					console.log(obj);
 					$rootScope.routeObj = obj;
